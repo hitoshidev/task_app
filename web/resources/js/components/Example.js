b9601441b30ea6c0a446900c190652cd7db94669
root <root@DESKTOP-5T6AEHG.localdomain>
2022-04-11 01:06:59 +0900

    add react

---
+++ web/resources/js/components/Example.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+function Example() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Example Component</div>
+
+                        <div className="card-body">I'm an example component!</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Example;
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<Example tab="home" />);
