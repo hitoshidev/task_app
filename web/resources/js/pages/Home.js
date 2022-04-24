21457ad47ab4924d74b326b1df09513bb1452777
root <root@DESKTOP-5T6AEHG.localdomain>
2022-04-24 16:18:56 +0900

    add post form

--- web/resources/js/pages/Home.js
+++ web/resources/js/pages/Home.js
@@ -3,8 +3,11 @@ import React from 'react';
 import { Button, Card } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { useState, useEffect } from 'react';
-import MainTable from '../components/MainTable'
 import axios from 'axios';
+
+import MainTable from '../components/MainTable'
+import PostForm from '../components/PostForm'
+
 
 const useStyles = makeStyles((theme) => createStyles({
     card: {
@@ -21,6 +24,7 @@ function Home() {
     const classes = useStyles();
 
     const [posts, setPosts] = useState([]);
+    const [formData, setFormData] = useState({name:'', content:''});
     
     useEffect(() => {
         getPostsData();
@@ -29,14 +33,40 @@ function Home() {
     const getPostsData = () => {
         axios.get('api/posts')
             .then(response => {
-                console.log(response.status)
-                console.log(response.data);
                 setPosts(response.data);
             })
             .catch(() => {
                 console.log('error connect');
             });
     };
+
+    const inputChange = e => {
+        console.log('inputChange called');
+        const key = e.target.name;
+        const value = e.target.value;
+        formData[key] = value;
+        let data = Object.assign({}, formData);
+        setFormData(data);
+        console.log(formData);
+    };
+
+    const createPost = async() => {
+        if(formData == '') { return; }
+
+        await axios.post('/api/post/create', {
+            name: formData.name,
+            content: formData.content
+        })
+        .then(res => {
+            const tmp = posts;
+            tmp.push(res.data);
+            setPosts(tmp);
+            setFormData('');
+        })
+        .catch(err => {
+            console.log(error);
+        });
+    }
 
     let rows = [];
     posts.map((post) => 
@@ -56,6 +86,9 @@ function Home() {
                 <div className="col-md-10">
                     <div className="card">
                         <h1>タスク管理</h1>
+                        <Card className={classes.card}>
+                            <PostForm data={formData} inputChange={inputChange} btnFunc={createPost}/>
+                        </Card>
                         <Card className={classes.card}>
                             {/* テーブル部分の定義 */}
                             <MainTable headerList={headerList} rows={rows} />
