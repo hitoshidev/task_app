f7c88b3a75abf3417ba5ea8a247c892d925b739f
root <root@DESKTOP-5T6AEHG.localdomain>
2022-04-24 19:30:11 +0900

    add delete

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -20062,6 +20062,8 @@ var useStyles = (0,_material_ui_core_styles__WEBPACK_IMPORTED_MODULE_6__["defaul
 var headerList = ['名前', 'タスク内容', '編集', '完了'];
 
 function Home() {
+  var _this = this;
+
   var classes = useStyles();
 
   var _useState = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)([]),
@@ -20140,6 +20142,36 @@ function Home() {
     };
   }();
 
+  var deletePost = /*#__PURE__*/function () {
+    var _ref2 = _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee2(post) {
+      return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee2$(_context2) {
+        while (1) {
+          switch (_context2.prev = _context2.next) {
+            case 0:
+              _context2.next = 2;
+              return axios__WEBPACK_IMPORTED_MODULE_2___default().post('/api/delete', {
+                id: post.id
+              }).then(function (res) {
+                _this.setState({
+                  posts: res.posts
+                });
+              })["catch"](function (err) {
+                console.log(err);
+              });
+
+            case 2:
+            case "end":
+              return _context2.stop();
+          }
+        }
+      }, _callee2);
+    }));
+
+    return function deletePost(_x) {
+      return _ref2.apply(this, arguments);
+    };
+  }();
+
   var rows = [];
   posts.map(function (post) {
     return rows.push({
@@ -20154,6 +20186,10 @@ function Home() {
       deleteBtn: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_8__["default"], {
         color: "primary",
         variant: "contained",
+        href: "/",
+        onClick: function onClick() {
+          return deletePost(post);
+        },
         children: "\u5B8C\u4E86"
       })
     });
